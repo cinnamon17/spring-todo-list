d6df488450ec2d3bbce5c197b00a74377ba83ce4
Nelson Moncada <juniormoncada17@gmail.com>
2023-04-11 02:25:29 +0200

    first commit base app

---
+++ src/main/java/com/cinnamon/list/Configuration/MvcConfig.java
@@ -0,0 +1,9 @@
+package com.cinnamon.list.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MvcConfig {
+
+
+}
---
+++ src/test/java/com/cinnamon/list/ListServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinnamon.list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
