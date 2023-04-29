0b5846d4da6c34f3085e693d6ce3c8a04d1ebdf6
Nelson Moncada <juniormoncada17@gmail.com>
2023-04-29 17:29:25 +0200

    Base style for todo-list

--- src/main/java/com/cinnamon/list/Controller/HomeController.java
+++ src/main/java/com/cinnamon/list/Controller/HomeController.java
@@ -3,9 +3,6 @@ package com.cinnamon.list.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-import java.util.ArrayList;
-
 @Controller
 public class HomeController {
 
@@ -15,11 +12,6 @@ public class HomeController {
 
     @GetMapping("/home")
     public String name() {
-
-        List<String> names = new ArrayList<String>();
-
-        names.add("Nelson");
-        names.add("Leydi");
 
         return "index";
 
